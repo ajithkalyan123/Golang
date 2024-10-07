e1b389395c7e98d4479358c3a090c1d24e988604
ajithkalyan123 <112503114+ajithkalyan123@users.noreply.github.com>
2024-10-07 10:13:46 -0500

    Add files via upload

---
+++ calculator/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"calculator/calci"
+	"fmt"
+)
+
+func main() {
+	var a, b float64
+	var option int
+	var result float64
+
+	fmt.Println("Select an option: 1. Add, 2. Subtract, 3. Multiply, 4. Divide")
+	fmt.Print("Enter the option: ")
+	fmt.Scanln(&option)
+	fmt.Print("Enter the value of a: ")
+	fmt.Scan(&a)
+	fmt.Print("Enter the value of b: ")
+	fmt.Scan(&b)
+	if option == 1 {
+		result = calci.Add(a, b)
+		fmt.Printf("Result: %.2f\n", result)
+	} else if option == 2 {
+		result = calci.Subtract(a, b)
+		fmt.Printf("Result: %.2f\n", result)
+	} else if option == 3 {
+		result = calci.Multiply(a, b)
+		fmt.Printf("Result: %.2f\n", result)
+	} else if option == 4 {
+		result = calci.Divide(a, b)
+		fmt.Printf("Result: %.2f\n", result)
+	} else {
+		fmt.Println("error")
+	}
+}
